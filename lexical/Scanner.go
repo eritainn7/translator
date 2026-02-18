12efa8defd970e5e1615241d18ba7f17b13400f2
eritainn7 <eritainn7@gmail.com>
2026-02-18 12:23:18 +0300

    feat: create empty body for method Run() of Scanner

---
+++ lexical/Scanner.go
@@ -0,0 +1,15 @@
+package lexical
+
+type Scanner struct {
+	buffer        string
+	current_state state
+	prev_state    state
+}
+
+func (scanner Scanner) Run() string {
+	sequence_leksems := ""
+	/**
+	...handling text program
+	**/
+	return sequence_leksems
+}
