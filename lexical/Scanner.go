88560f31106dde61f784b6b3494270d197ba9450
eritainn7 <eritainn7@gmail.com>
2026-02-18 17:38:00 +0300

    feat: in method scanner.Run() need create main login in cycle for

--- lexical/Scanner.go
+++ lexical/Scanner.go
@@ -1,15 +1,36 @@
 package lexical
 
+import (
+	"translator/shared"
+)
+
 type Scanner struct {
-	buffer        string
-	current_state state
-	prev_state    state
+	buffer                   string
+	pair_current_prev_states [2]state
+	path_to_file             string
 }
 
-func (scanner Scanner) Run() string {
+func InitScanner(path_to_file string) *Scanner {
+	return &Scanner{
+		buffer:                   "",
+		pair_current_prev_states: [2]state{state("S")},
+		path_to_file:             path_to_file,
+	}
+}
+
+func (scanner *Scanner) Run() string {
 	sequence_leksems := ""
-	/**
-	...handling text program
-	**/
+
+	fileReader := shared.InitFileReader(scanner.path_to_file)
+	text_program := fileReader.GetTextFile()
+
+	for i, sym := range text_program {
+		//In this place all main logic!!!
+	}
+
 	return sequence_leksems
+}
+
+func (scanner *Scanner) clearBuffer() {
+	scanner.buffer = ""
 }
