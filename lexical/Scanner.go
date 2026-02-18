fa834192994bd140fb0e74969708bc3c35f20910
eritainn7 <eritainn7@gmail.com>
2026-02-19 02:57:32 +0300

    feat: finish line/ working main logic in cycle for....

--- lexical/Scanner.go
+++ lexical/Scanner.go
@@ -6,15 +6,17 @@ import (
 
 type Scanner struct {
 	buffer                   string
-	pair_current_prev_states [2]state
+	pair_current_prev_states map[string]state
 	path_to_file             string
 }
 
 func InitScanner(path_to_file string) *Scanner {
 	return &Scanner{
-		buffer:                   "",
-		pair_current_prev_states: [2]state{state("S")},
-		path_to_file:             path_to_file,
+		buffer: "",
+		pair_current_prev_states: map[string]state{
+			"current_state": state("S"),
+		},
+		path_to_file: path_to_file,
 	}
 }
 
@@ -24,8 +26,8 @@ func (scanner *Scanner) Run() string {
 	fileReader := shared.InitFileReader(scanner.path_to_file)
 	text_program := fileReader.GetTextFile()
 
-	for i, sym := range text_program {
-		//In this place all main logic!!!
+	for _, sym := range text_program {
+
 	}
 
 	return sequence_leksems
@@ -33,4 +35,46 @@ func (scanner *Scanner) Run() string {
 
 func (scanner *Scanner) clearBuffer() {
 	scanner.buffer = ""
+}
+
+func (scanner *Scanner) isLetter(symbol rune) bool {
+	ans := false
+
+	return ans
+}
+
+func (scanner *Scanner) isDigit(symbol rune) bool {
+	ans := false
+
+	return ans
+}
+
+func (scanner *Scanner) isDot(symbol rune) bool {
+	ans := false
+
+	return ans
+}
+
+func (scanner *Scanner) isSlash(symbol rune) bool {
+	ans := false
+
+	return ans
+}
+
+func (scanner *Scanner) isSeparator(symbol rune) bool {
+	ans := false
+
+	return ans
+}
+
+func (scanner *Scanner) isLessThanSym(symbol rune) bool {
+	ans := false
+
+	return ans
+}
+
+func (scanner *Scanner) isGreaterThanSym(symbol rune) bool {
+	ans := false
+
+	return ans
 }
